72905cfb53d9604c6500b25031cfd576d7f8406a
gabrieleqwe3-droid <gabrieleqwe3@gmail.com>
2025-10-26 15:49:53 +0000

    xadrez2

--- xadrez.c
+++ xadrez.c
@@ -10,17 +10,15 @@ int main() {
     }
 
     printf("\n");
-
     printf("Movimento do BISPO:\n");
 
-    int j = 1; 
+    int j = 1;
     while (j <= 5) {
         printf("Cima, Direita\n");
-        j++; 
+        j++;
     }
 
     printf("\n");
-
     printf("Movimento da RAINHA:\n");
 
     int k = 1; 
@@ -28,6 +26,24 @@ int main() {
         printf("Esquerda\n");
         k++; 
     } while (k <= 8); 
+
+    printf("\n");
+    printf("Movimento Cavalo: \n"); 
+
+    for (int a = 1; a <= 2; a++){
+        printf("Vertical \n");
+
+        if (a == 2) {
+            int b = 1;
+           while (b <= 1)
+        {
+            printf("Horizontal");
+            b++;
+        }
+        printf("\n");
+    }
+        
+    }
 
     printf("\nSimulação concluída com sucesso!\n");
 
